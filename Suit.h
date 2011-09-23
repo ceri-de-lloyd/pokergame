44f21e0c95c969861f5ff8e77decfdccc6f5c874
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-09-23 15:08:20 +0100

    Further improvements.
    determine_index indeed is static, and the constants need to be
    publically available.

--- Suit.h
+++ Suit.h
@@ -9,14 +9,13 @@ class Suit {
   // Wrapper class around suites as integer indices.
  public :
   typedef int index_type;
-
- private :
   static const index_type num_suites = 4;
   static const index_type clubs = 0;
   static const index_type spades = 1;
   static const index_type hearts = 2;
   static const index_type diamonds = 3;
 
+ private :
   static std::string suit_names(const index_type i) {
     assert(i >= 0);
     assert(i < num_suites);
@@ -27,6 +26,11 @@ class Suit {
     case diamonds : return "diamonds";
     }
   }
+  index_type determine_index(const std::string& str) const {
+    for (index_type i = 0; i < num_suites; ++i)
+      if (str == suit_names(i)) return i;
+    throw std::domain_error("Does not represent a suit: " + str);
+  }
 
  public :
 
@@ -36,6 +40,7 @@ class Suit {
     assert(i >= 0);
     assert(i < num_suites);
   }
+  //! this constructor throws std::domain_error
   Suit(const std::string& str) : index(determine_index(str)) {}
 
   friend bool operator ==(const Suit lhs, const Suit rhs) {
@@ -43,11 +48,6 @@ class Suit {
   }
   operator std::string { return suit_names(index); }
 
-  index_type determine_index(const std::string& str) const {
-    for (index_type i = 0; i < num_suites; ++i)
-      if (str == suit_names(i)) return i;
-    throw std::domain_error("Does not represent a suit: " + str);
-  }
 };
 
 #endif
